bcb099f897d2cb8131f057dbd177104974e1c82f
Santiago MVO <santiago.mvao@gmail.com>
2019-12-03 13:43:52 -0600

    added initial friends.js data, added html appearance

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+// Initial Possible Friends Data
+var friends = [
+    {
+        name:"Travis",
+        photo:"https://randomuser.me/api/portraits/men/73.jpg",
+        scores:[4,3,5,1,4,5,4,1,3,5]
+    },
+    {
+        name:"Linda",
+        photo:"https://randomuser.me/api/portraits/women/37.jpg",
+        scores:[1,4,5,3,1,3,5,4,2,3]
+    },
+    {
+        name:"Angela",
+        photo:"https://randomuser.me/api/portraits/women/30.jpg",
+        scores:[1,4,2,3,4,5,5,2,3,1]
+    },
+    {
+        name:"Kevin",
+        photo:"https://randomuser.me/api/portraits/men/19.jpg",
+        scores:[1,3,5,4,2,3,2,2,4,5]
+    },
+    {
+        name:"Emmet",
+        photo:"https://randomuser.me/api/portraits/lego/1.jpg",
+        scores:[3,2,5,4,5,4,1,1,2,5]
+    },
+];
+
+//Exporting Data
+module.exports = friends;
