1d428fa8e1e06822ec3319c8ac391ff03da503bd
Santiago MVO <santiago.mvao@gmail.com>
2019-12-04 20:08:16 -0600

    added navbar files and links to them

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/navbar'));
 
 // Routers
 require("./app/routing/apiRoutes")(app);
